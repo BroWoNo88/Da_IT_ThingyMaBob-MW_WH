0602baff5fba2442ce915c4c7977d17a4bbc3443
BroWoNo88 <matthewwedding00@gmail.com>
2026-08-27 09:16:53 +1000

    Defining the stuffs
    👍 defined all the stuff cos  👍

---
+++ Da_Buzzer.py
@@ -0,0 +1,7 @@
+from machine import Pin
+led = Pin(0, Pin.OUT) #only for single colour
+buzz = Pin(1, Pin.OUT) #high pitch buzz without resistor or other coding
+RGB_Red = Pin(2, Pin.OUT)
+RGB_Green = Pin(3, Pin.OUT)
+RGB_Blue = Pin(4, Pin.OUT
+#change the pin calls if you want 👍
