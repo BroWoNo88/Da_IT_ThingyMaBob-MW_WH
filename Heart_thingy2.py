e2c34f45c1025e811baa14875c6596792369b000
BroWoNo88 <matthewwedding00@gmail.com>
2026-09-04 13:31:51 +1000

    Create Heart_thingy2.py

---
+++ Heart_thingy2.py
@@ -0,0 +1,27 @@
+# matthews heart beat thingy numba 2
+import time
+import random
+from machine import Pin
+heart_rate = 80
+led = Pin(0, Pin.OUT)
+buzz = Pin(1, Pin.OUT)
+def check_heart_rate(heart_rate):
+    change = random.randint(-5, 5) 
+    if heart_rate < 80:
+        change += 2.0 
+        heart_rate += change
+    elif heart_rate > 80:
+        change -= 2.0
+        heart_rate += change
+
+while True:
+    check_heart_rate()
+    print("Resting Heart rate is", round(heart_rate), "BPM")
+    print(" \n \n \n \n \n \n \n \n \n \n \n \n ")
+    led.on()
+    buzz.on()
+    time.sleep(0.05)
+    buzz.off()
+    led.off()
+    time.sleep(heart_rate/60) #currently has a check based on heart rate, so bad
+    print heart rate
