d69a5457900eba03afbab7437a11f2896febe8d9
BroWoNo88 <matthewwedding00@gmail.com>
2026-09-08 11:37:09 +1000

    Add heart rate monitoring functionality
    Implement heart rate monitoring with LED and buzzer feedback.

---
+++ Heartrate#3.py
@@ -0,0 +1,37 @@
+# matthews heart beat thingy numba 1
+import time
+import random
+from machine import Pin
+heart_rate = 80
+led = Pin(0, Pin.OUT)
+buzz = Pin(1, Pin.OUT)
+red = Pin(2, Pin.OUT)
+green = Pin(3, Pin.OUT)
+def check_heart_rate():
+    global heart_rate
+    change = random.randint(-10, 50) 
+    if heart_rate < 80:
+        change += 2.0 
+        heart_rate += change
+    elif heart_rate > 80:
+        change -= 2.0
+        heart_rate += change
+    else:
+        heart_rate += change
+
+while True:
+    check_heart_rate()
+    print("Resting Heart rate is", round(heart_rate), "BPM")
+    print(" \n \n \n \n \n \n \n \n \n \n \n \n ")
+    led.on()
+    buzz.on()
+    time.sleep(0.05)
+    buzz.off()
+    led.off()
+    time.sleep(60/heart_rate) #currently has a check based on heart rate, so bad`
+    if heart_rate > 110 or heart_rate < 50:
+        red.on()
+        green.off()
+    else:
+        red.off()
+        green.on()
